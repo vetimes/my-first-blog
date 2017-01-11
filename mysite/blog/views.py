bcc4dcdf5666273d2c1bfd0a971d5737d689577c
xxvetimesxx <xxvetimesxx@gmail.com>
2017-01-11 00:55:19 -0800

    두번쨰 커밋 뷰에 문제 있음 수정요망

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,9 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),  # 블로그로 끝나는 url을 그 뒤는 blog폴더의 url파일에 나머지를 맡긴다
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
 ]
