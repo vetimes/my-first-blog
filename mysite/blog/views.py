57a13b8137f24ffa5df0606663677db791cbd17b
xxvetimesxx <xxvetimesxx@gmail.com>
2017-01-11 19:02:36 -0800

    blog bug fix

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,16 +4,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in page
+    paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html ', {'page': page,
-                                                    'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
