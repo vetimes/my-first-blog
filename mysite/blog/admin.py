483e7e3153cf306ba96a8d8ad0dbcbad3b57ff0c
xxvetimesxx <xxvetimesxx@gmail.com>
2017-01-12 00:25:06 -0800

    add commentform to views, not template

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+admin.site.register(Comment, CommentAdmin)
 
 
 class PostAdmin(admin.ModelAdmin):
